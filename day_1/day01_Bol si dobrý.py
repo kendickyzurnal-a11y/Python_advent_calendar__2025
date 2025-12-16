6ae8628903c1e5441eac964aee41a908e7cdb6c7
kendickyzurnal-a11y <kendickyzurnal@gmail.com>
2025-12-16 16:36:54 +0100

    Added right answer

---
+++ day_1/day01_Bol si dobrý.py
@@ -0,0 +1,10 @@
+skutky = int(input("Koľko dobrých skutkov si urobil? "))
+
+# sem doplň if / elif / else
+
+if skutky == 0:
+    print ("Uhlie!")
+elif skutky > 0 and skutky < 5:
+    print ("Malý darček 🎁!")
+else:
+    print ("Veľký dar 🎄🎁!")
