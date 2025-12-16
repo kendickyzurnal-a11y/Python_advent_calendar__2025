a8b72665e34a827bef6c5c707132d0460c3de04e
kendickyzurnal-a11y <kendickyzurnal@gmail.com>
2025-12-16 16:46:16 +0100

    Instructions

---
+++ day_1/day01.py
@@ -0,0 +1,12 @@
+skutky = int(input("Koľko dobrých skutkov si urobil? "))
+
+# sem doplň if / elif / else
+# ak je:
+
+#0 → „Uhlie 🪨“
+
+#1 až 4 → „Malý darček 🎁“
+
+#5 a viac → „Veľký dar 🎄🎁“
+
+#vypíše správu
