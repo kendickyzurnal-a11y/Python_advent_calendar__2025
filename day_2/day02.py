4f7b694a311a6ce2489690756ca2f958863ffd02
kendickyzurnal-a11y <kendickyzurnal@gmail.com>
2025-12-17 16:44:11 +0100

    Upload instructions file to day2

---
+++ day_2/day02.py
@@ -0,0 +1,11 @@
+from random import randint
+
+pocet = int(input("Koľko má stromček svetielok? "))
+
+svieti = 0
+
+for i in range(pocet):
+    # doplň kód
+    pass
+
+print("Svieti", svieti, "svetielok 🎄")
