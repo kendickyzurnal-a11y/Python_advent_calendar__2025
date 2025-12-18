b8656255d7bbf918524b75f80b50556067a1d231
kendickyzurnal-a11y <kendickyzurnal@gmail.com>
2025-12-18 15:17:58 +0100

    Upload INSTRUCTIONS FILE

---
+++ day_3/day03.py
@@ -0,0 +1,12 @@
+from random import randint
+
+darceky = [
+    # doplň darčeky
+]
+
+for i in range(len(darceky)):
+    # vypíš poradové číslo a názov darčeka
+    pass
+
+nahodny = randint(0, len(darceky) - 1)
+print("🎁 Dnes dostávaš:", darceky[nahodny])
